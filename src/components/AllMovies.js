39fb2e31951f4aaa8d1fb62cc451570d617a807c
TatyCat <williams.tatyanna@gmail.com>
2019-02-20 16:24:38 -0500

    Added dynamic alt tag

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import movieData from '../data/moviedata.json'
 
+
 class AllMovies extends Component {
   render() {
     return (
       < main >
         {movieData.results.map((eachMovieDataItem) => {
+          // console.log()
           return (
             <section key={eachMovieDataItem.id}>
               <h2>{eachMovieDataItem.title}</h2>
-              <h3>{eachMovieDataItem.release_date}</h3>
-              <img src={'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + eachMovieDataItem.poster_path} />
+              <h3>({eachMovieDataItem.release_date})</h3>
+              <img src={'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + eachMovieDataItem.poster_path}
+                alt={'Poster image of the movie ' + eachMovieDataItem.title}
+              />
+
               <p>{eachMovieDataItem.overview}</p>
-              <p>{eachMovieDataItem.vote_average}</p>
+              <p>AVG Vote: {eachMovieDataItem.vote_average}</p>
             </section>
           )
         })
