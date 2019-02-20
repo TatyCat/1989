661158ea6228e32190ac7486956a1463768cb927
TatyCat <williams.tatyanna@gmail.com>
2019-02-20 16:53:48 -0500

    Added 1989 stying and reformated the date

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -8,14 +8,14 @@ class AllMovies extends Component {
       < main >
         {movieData.results.map((eachMovieDataItem) => {
           // console.log()
+          // console.log()
           return (
             <section key={eachMovieDataItem.id}>
               <h2>{eachMovieDataItem.title}</h2>
-              <h3>({eachMovieDataItem.release_date})</h3>
               <img src={'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + eachMovieDataItem.poster_path}
                 alt={'Poster image of the movie ' + eachMovieDataItem.title}
               />
-
+              <h3>{eachMovieDataItem.release_date.split('-')[1] + "-" + eachMovieDataItem.release_date.split('-')[2] + "-" + eachMovieDataItem.release_date.split('-')[0]}</h3>
               <p>{eachMovieDataItem.overview}</p>
               <p>AVG Vote: {eachMovieDataItem.vote_average}</p>
             </section>
